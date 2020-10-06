cc228c09c560e9db990354ece76e73997deeafea
davincikab <davidnganganjeri079@gmail.com>
2020-10-06 17:55:30 +0300

    updated static files

---
+++ disaster/static/js/main.js
@@ -0,0 +1,4 @@
+var map = L.map('map', {
+    center: [0.099242, 34.051121],
+    zoom: 13,
+});  
---
+++ disaster/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import home_view
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path("", home_view, name="home")
+]
+
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
